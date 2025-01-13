019f834a1e436b38db5964953061acb83bc4f9a5
Cevdet Karakulak <cevdetkarakulak_83@hotmail.com>
2025-01-13 23:45:44 +0300

    update

---
+++ CSharpEgitimKampi301.EFProject/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CSharpEgitimKampi301.EFProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnGetById = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rehber Id:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(85, 22);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(149, 20);
+            this.txtId.TabIndex = 3;
+            // 
+            // btnList
+            // 
+            this.btnList.BackColor = System.Drawing.Color.Orange;
+            this.btnList.Location = new System.Drawing.Point(85, 100);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(149, 32);
+            this.btnList.TabIndex = 4;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(240, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(467, 264);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(85, 48);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(149, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Rehber Adı:";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(85, 74);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(149, 20);
+            this.txtSurname.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Rehber Soyadı:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Orange;
+            this.btnAdd.Location = new System.Drawing.Point(85, 138);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(149, 32);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Orange;
+            this.btnDelete.Location = new System.Drawing.Point(85, 176);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(149, 32);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Orange;
+            this.btnUpdate.Location = new System.Drawing.Point(85, 214);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(149, 32);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnGetById
+            // 
+            this.btnGetById.BackColor = System.Drawing.Color.Orange;
+            this.btnGetById.Location = new System.Drawing.Point(85, 252);
+            this.btnGetById.Name = "btnGetById";
+            this.btnGetById.Size = new System.Drawing.Size(149, 32);
+            this.btnGetById.TabIndex = 13;
+            this.btnGetById.Text = "Id\'ye Göre Getir";
+            this.btnGetById.UseVisualStyleBackColor = false;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 296);
+            this.Controls.Add(this.btnGetById);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnGetById;
+    }
+}
+
---
+++ CSharpEgitimKampi301.EFProject/FrmLocation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+        }
+        EgitimEfTravelDbEntities dbEntities = new EgitimEfTravelDbEntities();
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values=dbEntities.Location.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void FrmLocation_Load(object sender, EventArgs e)
+        {
+            var values=dbEntities.Guide.Select(x=>new
+            {
+                FullName=x.GuideName+" "+x.GuideSurname,x.GuideId
+            }).ToList();
+            cmbGuide.DisplayMember = "FullName";
+            cmbGuide.ValueMember = "GuideId";
+            cmbGuide.DataSource= values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Location location=new Location();
+            location.Capacity=byte.Parse(nudCapasity.Value.ToString());
+            location.City=txtCity.Text;
+            location.Country=txtCountry.Text;
+            location.Price=decimal.Parse(txtPrice.Text);
+            location.DayNight=txtDayNight.Text;
+            location.GuideId=int.Parse(cmbGuide.SelectedValue.ToString());
+            dbEntities.Location.Add(location);
+            dbEntities.SaveChanges();
+            MessageBox.Show("Ekleme işlemi başarılı");
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtId.Text);
+            var deletedvalue = dbEntities.Location.Find(id);
+            dbEntities.Location.Remove(deletedvalue);
+            dbEntities.SaveChanges();
+            MessageBox.Show("Sİlme işlemi başarılı");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtId.Text);
+            var updatedvalue = dbEntities.Location.Find(id);
+            updatedvalue.DayNight = txtDayNight.Text;
+            updatedvalue.Price = decimal.Parse(txtPrice.Text);
+            updatedvalue.Capacity=byte.Parse(nudCapasity.Value.ToString());
+            updatedvalue.City=txtCity.Text;
+            updatedvalue.Country=txtCountry.Text;
+            updatedvalue.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+            dbEntities.SaveChanges();
+            MessageBox.Show("Güncelleme İşlemi Başarılı");
+        }
+    }
+}
